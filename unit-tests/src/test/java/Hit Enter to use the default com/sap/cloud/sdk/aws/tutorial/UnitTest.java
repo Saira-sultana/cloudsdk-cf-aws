c24cb4a9e634ca5bfa526d5ca1e50d31e658be49
SAPTechEd2019ain369 <52492960+SAPTechEd2019ain369@users.noreply.github.com>
2019-11-14 14:10:19 +0530

    Initial version

---
+++ unit-tests/src/test/java/Hit Enter to use the default com/sap/cloud/sdk/aws/tutorial/UnitTest.java
@@ -0,0 +1,12 @@
+package Hit Enter to use the default com.sap.cloud.sdk.aws.tutorial;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UnitTest {
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
